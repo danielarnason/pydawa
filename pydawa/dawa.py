3b0553b42a1f3dead3150ee8744db3784b5190f0
Daníel Örn Árnason <danielarnason85@gmail.com>
2018-10-04 15:36:20 +0200

    Adressesoeg docstring tilføjet

--- pydawa/dawa.py
+++ pydawa/dawa.py
@@ -4,17 +4,30 @@ from urllib.parse import urlencode
 
 @dataclass
 class Adressesoeg:
+    """Søg efter en adresse i DAR.
+
+        :param vejnavn: Vejnavnet på den søgte adresse.
+        :param husnr: Husnummeret på den søgte adresse.
+        :param postnr: Postnummeret på den søgte adresse.
+        :param struktur: Strukturen af returneret data fra API. Mulige værdier: "nestet", "flad" eller "mini". Det anbefales at benytte mini-formatet hvis der ikke er behov for den fulde struktur, da dette vil give bedre svartider (DEFAULT mini)
+        :param srid: SRID for det koordinatsystem koordinaterne er i (DEFAULT 25832).
+        :param format: Mulige værdier json (DEFAULT), geojson (hvor det giver mening), jsonp, ndjson, csv.
+
+        :returns: En dictionary med svar fra API.
+        :rtype: dictionary
+    """
     vejnavn: str
     husnr: str
     postnr: str
-    url: str = 'http://dawa.aws.dk/adresser'
     struktur: str = 'mini'
     srid: str = '25832'
     format: str = 'json'
 
     def info(self):
+        """Henter information om adressen fra DAWA API.
+        """
         params = urlencode({'vejnavn': self.vejnavn, 'husnr': self.husnr, 'postnr': self.postnr, 'struktur': self.struktur, 'format': self.format})
-        url = self.url + '?' + params
+        url = 'http://dawa.aws.dk/adresser?' + params
         response = requests.get(url)
         if self.format == 'json' or self.format == 'geojson':
             response = response.json()

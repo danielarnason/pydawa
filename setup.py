253deafc6349eb687af4f1d56b72efb23752463a
Daníel Örn Árnason <danielarnason85@gmail.com>
2018-11-17 14:34:16 +0100

    setup.py tilføjet

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pydawa',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT',
+    description='API wrapper til DAWA',
+    long_description=open('README.md').read(),
+    install_requires=['requests'],
+    url='http://github.com/danielarnason/pydawa',
+    author='Daníel Örn Árnason',
+    author_email='danielarnason85@gmail.com'
+)

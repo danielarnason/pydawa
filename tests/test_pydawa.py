7a36b504d2f85443f26b353439cb41902c440b08
danielarnason <danielarnason85@gmail.com>
2019-02-13 10:25:00 +0100

    tests ændret

--- tests/test_pydawa.py
+++ tests/test_pydawa.py
@@ -2,7 +2,7 @@ import pytest
 from pydawa import Adressesoeg, Adresseopslag, Adressevasker
 
 def test_adressesoeg():
-    adresse = Adressesoeg('Rødkildevej', '46', '2400')
+    adresse = Adressesoeg(vejnavn='Rødkildevej', husnr='46', postnr='2400')
     adresse_info = adresse.info()
     assert isinstance(adresse_info, list)
     assert adresse_info[0]['vejnavn'] == 'Rødkildevej'
@@ -10,7 +10,7 @@ def test_adressesoeg():
     assert adresse_info[0]['postnr'] == '2400'
 
 def test_adresseopslag():
-    adresse = Adresseopslag('0255b942-f3ac-4969-a963-d2c4ed9ab943')
+    adresse = Adresseopslag(id='0255b942-f3ac-4969-a963-d2c4ed9ab943')
     adresse_info = adresse.info()
     assert isinstance(adresse_info, dict)
     assert adresse_info['vejnavn'] == 'Mosestien'
@@ -18,7 +18,7 @@ def test_adresseopslag():
     assert adresse_info['postnr'] == '6430'
 
 def test_adressevasker():
-    adresse = Adressevasker('Dronning dagmarsvej 200 3650 ølstyke')
+    adresse = Adressevasker(betegnelse='Dronning dagmarsvej 200 3650 ølstyke')
     adresse_info = adresse.info()
     assert isinstance(adresse_info, dict)
     assert 'kategori' in adresse_info

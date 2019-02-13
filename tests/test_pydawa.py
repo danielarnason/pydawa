9d128b841070e2c5f4a36f239778a12abe82d19b
danielarnason <danielarnason85@gmail.com>
2019-02-13 12:14:53 +0100

    koordinat test tilføjet

--- tests/test_pydawa.py
+++ tests/test_pydawa.py
@@ -1,8 +1,16 @@
 import pytest
 from pydawa import Adressesoeg, Adresseopslag, Adressevasker
 
-def test_adressesoeg():
+def test_adressesoeg_opslit():
     adresse = Adressesoeg(vejnavn='Rødkildevej', husnr='46', postnr='2400')
+    adresse_info = adresse.info()
+    assert isinstance(adresse_info, list)
+    assert adresse_info[0]['vejnavn'] == 'Rødkildevej'
+    assert adresse_info[0]['husnr'] == '46'
+    assert adresse_info[0]['postnr'] == '2400'
+
+def test_adressesoeg_q():
+    adresse = Adressesoeg(q='Rødkildevej 46, 2400 København')
     adresse_info = adresse.info()
     assert isinstance(adresse_info, list)
     assert adresse_info[0]['vejnavn'] == 'Rødkildevej'
@@ -23,3 +31,11 @@ def test_adressevasker():
     assert isinstance(adresse_info, dict)
     assert 'kategori' in adresse_info
     assert isinstance(adresse_info['resultater'], list)
+
+def test_get_koordinater():
+    adresse = Adressesoeg(q='Rødkildevej 46, 2400 København')
+    adresse_info = adresse.info()
+    koordinater = adresse.koordinater(adresse_info[0])
+    assert isinstance(koordinater, tuple)
+    assert isinstance(koordinater[0], float)
+    assert isinstance(koordinater[1], float)

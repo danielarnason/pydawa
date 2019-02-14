8cfabf46b97b5ee27ea2f4450d14f9f7fbcff970
danielarnason <danielarnason85@gmail.com>
2019-02-14 17:23:18 +0100

    Travis test :)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydawa',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     license='MIT',
     description='API wrapper til DAWA',

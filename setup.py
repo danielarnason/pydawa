04f51cc857cbf02beaa470458fe670caffda022c
Daníel Örn Árnason <danielarnason85@gmail.com>
2019-02-15 11:53:51 +0100

    versions nr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydawa',
-    version='0.2.2',
+    version='0.3.0',
     packages=find_packages(),
     license='MIT',
     description='API wrapper til DAWA',

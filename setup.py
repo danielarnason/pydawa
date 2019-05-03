f5a194fc2e341a37994e1601ef769fdd88f0b957
Daníel Örn Árnason <danielarnason85@gmail.com>
2019-05-03 10:13:35 +0200

    geokod df function taget ud af geokod file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydawa',
-    version='0.4.0',
+    version='0.4.1',
     packages=find_packages(),
     license='MIT',
     description='API wrapper til DAWA',
